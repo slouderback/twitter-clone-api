658d9ebc36fd5841d9ef03b4f6e86808570436f4
slouderback <sam@louderback.com>
2020-12-11 01:48:27 -0800

    make tweets route

--- models/tweet.model.js
+++ models/tweet.model.js
@@ -1,15 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const tweetSchema = new Schema({
-  userID: { type: String, required: true },
-  // date: { type: Date, required: true },
-  tweetBody: { type: String, required: true },
-}, {
-  timestamps: true,
-});
+const tweetSchema = new Schema(
+  {
+    userID: { type: String, required: true },
+    userName: { type: String, required: true },
+    userHandle: { type: String, required: true },
+    // date: { type: Date, required: true },
+    tweetBody: { type: String, required: true },
+    images: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Tweet = mongoose.model('Tweet', tweetSchema);
+const Tweet = mongoose.model("Tweet", tweetSchema);
 
 module.exports = Tweet;
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,26 +1,32 @@
-const router = require('express').Router();
-let Tweet = require('../models/tweet.model');
+const router = require("express").Router();
+let Tweet = require("../models/tweet.model");
 
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   Tweet.find()
-    .then(tweets => res.json(tweets))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .then((tweets) => res.json(tweets))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/add').post((req, res) => {
+router.route("/add").post((req, res) => {
   const userID = req.body.userID;
+  const userName = req.body.userName;
+  const userHandle = req.body.userHandle;
   // const date = Date.parse(req.body.date);
   const tweetBody = req.body.tweetBody;
+  const images = req.body.images;
 
   const newTweet = new Tweet({
     userID,
-    // date,
+    userName,
+    userHandle,
     tweetBody,
+    images,
   });
 
-  newTweet.save()
-  .then(() => res.json('Tweet added!'))
-  .catch(err => res.status(400).json('Error: ' + err));
+  newTweet
+    .save()
+    .then(() => res.json("Tweet added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
