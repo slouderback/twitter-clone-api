9ab36e979e474c0c7435f23fe41e61552c78122c
Osama Hanhan <hanhan.osama@gmail.com>
2020-12-13 07:21:30 -0800

    added to tweet model and user model

--- models/tweet.model.js
+++ models/tweet.model.js
@@ -4,11 +4,21 @@ const Schema = mongoose.Schema;
 
 const tweetSchema = new Schema(
   {
+    tweetUUID: { type: String, required: true },
     userID: { type: String, required: true },
     userName: { type: String, required: true },
     userHandle: { type: String, required: true },
     // date: { type: Date, required: true },
     tweetBody: { type: String, required: true },
+
+    likedBy: [{ userUUID: { type: String }, date: { type: Date } }], //storing likes would be using an array of jsons. Need date for showing liked tweets
+
+    //this may not be the best way to do this. Some questions are: how do we store likes of the retweet? Perhaps we make the retweet 
+    //a tweet in and of itself and store it with the value 'retweet'; can also have value 'comment' or 'tweet' and shit like that?
+    //Perhaps we store only uuid and date here and then in the user's model, we store tweetsRetweeted and that holds a json of a retweet and
+    //the likes and comments it got? 
+    retweetedBy: [{ userUUID: { type: String }, date: { type: Date } }], //holds who retweeted, and the date it was retweeted
+    sharedBy: [{ userUUID: { type: String }, Date: { type: Date }, sharedThrough: { type: String } }], //sharedThrough holds whether it was the 'dm' or 'added to bookmarks' or 'copy link' options
     images: [
       {
         type: String,
--- models/user.model.js
+++ models/user.model.js
@@ -6,12 +6,32 @@ const userSchema = new Schema(
 	{
 		username: {
 			type: String,
-			required: true,
+			required: true, //username here encompasses email, username, and phonenumber. 
 			unique: true,
 			trim: true,
 			minlength: 3,
 		},
 		password: { type: String, required: true },
+		birthdate: { type: String, required: true }, //can store it as a string with delimiters for one query access
+		name: { type: String, required: true }, // screen name
+
+		tweetsMade: [{ tweetUUID: { type: String } }], //array of tweetUUIDs since we want all the tweets a use has made
+		retweets: [{ tweetUUID: { type: String } }], //same as tweetsMade but only for retweets. Retweets ARE TWEETS but sometimes content-less
+		draftedTweets: [{ content: { type: String }, dateDrafted: { type: Date } }],
+		likedTweets: [{ tweetUUID: { type: String }, date: { type: Date } }],
+		usersFollowing: [
+			{
+				userUUID: { type: String }, date: { type: Date },
+				isMuted: { type: Boolean }, notificationsOn: { type: Boolean }
+			}],
+		followedByUsers: [{ userUUID: { type: String }, date: { type: Date } }],
+		blockedUsers: [{ userUUID: { type: String }, date: { type: Date } }],
+		bookmarkedTweets: [{ tweetUUID: { type: String }, date: { type: Date } }],
+		profilePhotoUrl: { type: String },
+		coverPhotoUrl: { type: String },
+
+		//need to store messaging data but idk how to do that just yet
+
 	},
 	{
 		timestamps: true,
